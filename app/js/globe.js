21f01aa225bc57db50e41a988f7c27c34bdf1053
Isaac Siegel <siegelhorn@gmail.com>
2016-02-26 12:13:16 -0800

    Added shaders to improve visibility of darker globes

--- app/js/globe.js
+++ app/js/globe.js
@@ -87,6 +87,9 @@ var Globe = function(container, urls) {
 
         // Add meshes to scene
         scene.add(earth);
+
+
+
         //scene.remove(earth);
         scene.add(createMesh.atmosphere());
 
@@ -95,9 +98,13 @@ var Globe = function(container, urls) {
          //scene.add(light);
 
         var geometry  = new THREE.SphereGeometry(600, 64, 64) ;
+
         var material  = new THREE.MeshBasicMaterial();
         material.map   = THREE.ImageUtils.loadTexture('./img/galaxy_starfield.png');
         material.side  = THREE.BackSide;
+
+
+
         var mesh  = new THREE.Mesh(geometry, material)  ;
         scene.add(mesh)
 
@@ -165,21 +172,59 @@ var Globe = function(container, urls) {
         // @link http://learningthreejs.com/blog/2013/09/16/how-to-make-the-earth-in-webgl/
         // @link http://learningthreejs.com/data/2013-09-16-how-to-make-the-earth-in-webgl/demo/index.html
         earth: function(urls) {
-            if (!urls.earth)
-                throw 'No image URL provided for an earth image';
+            //if (!urls.earth)
+            //    throw 'No image URL provided for an earth image';
+            //
+            //var material = new THREE.MeshPhongMaterial();
+            //material.map = THREE.ImageUtils.loadTexture(urls.earth);
+            //
+            //if (urls.bump) {
+            //    material.bump = THREE.ImageUtils.loadTexture(urls.bump);
+            //    material.bumpScale = 0.02;
+            //}
+            //
+            //if (urls.specular) {
+            //    material.specularMap = THREE.ImageUtils.loadTexture(urls.specular);
+            //    material.specular = new THREE.Color('grey');
+            //}
 
-            var material = new THREE.MeshPhongMaterial();
-            material.map = THREE.ImageUtils.loadTexture(urls.earth);
+            var shader = {
+                uniforms: {
+                    'texture': { type: 't', value: null }
+                },
+                vertexShader: [
+                    'varying vec3 vNormal;',
+                    'varying vec2 vUv;',
+                    'void main() {',
+                    'gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );',
+                    'vNormal = normalize( normalMatrix * normal );',
+                    'vUv = uv;',
+                    '}'
+                ].join('\n'),
+                fragmentShader: [
+                    'uniform sampler2D texture;',
+                    'varying vec3 vNormal;',
+                    'varying vec2 vUv;',
+                    'void main() {',
+                    'vec3 diffuse = texture2D( texture, vUv ).xyz;',
+                    'float intensity = 1.05 - dot( vNormal, vec3( 0.0, 0.0, 1.0 ) );',
+                    'vec3 atmosphere = vec3( 1.0, 1.0, 1.0 ) * pow( intensity, 3.0 );',
+                    'gl_FragColor = vec4( diffuse + atmosphere, 1.0 );',
+                    '}'
+                ].join('\n')
+            };
 
-            if (urls.bump) {
-                material.bump = THREE.ImageUtils.loadTexture(urls.bump);
-                material.bumpScale = 0.02;
-            }
+            var uniforms = THREE.UniformsUtils.clone(shader.uniforms);
+            uniforms['texture'].value = THREE.ImageUtils.loadTexture(urls.earth);
 
-            if (urls.specular) {
-                material.specularMap = THREE.ImageUtils.loadTexture(urls.specular);
-                material.specular = new THREE.Color('grey');
-            }
+            var material = new THREE.ShaderMaterial({
+
+                uniforms: uniforms,
+                vertexShader: shader.vertexShader,
+                fragmentShader: shader.fragmentShader
+
+            });
+
 
             return new THREE.Mesh(earthGeometry, material);
         },
@@ -191,20 +236,6 @@ var Globe = function(container, urls) {
         // Currently has some issues, especially when zooming out (distance > 900)
         atmosphere: function() {
             var material = new THREE.ShaderMaterial({
-                //vertexShader: [
-                // 'varying vec3 vNormal;',
-                // 'void main() {',
-                //   'vNormal = normalize( normalMatrix * normal );',
-                //   'gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );',
-                // '}'
-                //].join('\n'),
-                //fragmentShader: [
-                // 'varying vec3 vNormal;',
-                // 'void main() {',
-                //   'float intensity = pow( 0.8 - dot( vNormal, vec3( 0, 0, 1.0 ) ), 7.0 );',
-                //   'gl_FragColor = vec4( 0, 0.18, 1.0, 1.0 ) * intensity;',
-                // '}'
-                //].join('\n'),
                 vertexShader: [
                     'varying vec3 vNormal;',
                     'void main() {',
